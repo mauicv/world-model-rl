ffe8973e102438a4ca6983dd1461bf46dfcd57fc
Alex Athorne <a.thornysort@gmail.com>
2024-07-06 15:07:58 +0100

    Add test for NormalNoise repeat

--- src/reflect/data/tests/test_noise.py
+++ src/reflect/data/tests/test_noise.py
@@ -3,8 +3,9 @@ from reflect.models.world_model.observation_model import ObservationalModel
 from torchvision.transforms import Resize, Compose
 import gymnasium as gym
 import torch
+import numpy as np
 import pytest
-from reflect.data.noise import SmoothNoiseND, LinearSegmentNoiseND
+from reflect.data.noise import SmoothNoiseND, LinearSegmentNoiseND, NormalNoise
 
 def test_smooth_noise_generators():
     generator = SmoothNoiseND(
@@ -29,6 +30,18 @@ def test_linear_noise_generators():
     noise = generator()
     assert len(noise) == 8
 
+
+def test_normal_noise_generators():
+    generator = NormalNoise(
+        dim=2,
+        repeat=2
+    )
+    generator.reset()
+    noise1 = generator()
+    noise2 = generator()
+    assert np.all(noise1 == noise2)
+    noise3 = generator()
+    assert np.all(noise2 != noise3)
 
 
 @pytest.mark.parametrize("env_name", [
