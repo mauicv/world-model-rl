16fc1680b93e1bb1417c27c3aa58ff5503c51e3d
Alex Athorne <a.thornysort@gmail.com>
2024-07-06 17:09:26 +0100

    Add action repeat to actor model

--- src/reflect/models/rl/tests/test_actor.py
+++ src/reflect/models/rl/tests/test_actor.py
@@ -62,3 +62,28 @@ def test_actor_stochastic(env_name, batch_size):
     assert action.shape == (batch_size, gym_env.action_space.shape[0])
     assert torch.all(action >= torch.tensor(gym_env.action_space.low))
     assert torch.all(action <= torch.tensor(gym_env.action_space.high))
+
+
+@pytest.mark.parametrize("env_name,batch_size", [
+    ("InvertedPendulum-v4", 5),
+    ("Ant-v4", 5),
+])
+def test_actor_repeat(env_name, batch_size):
+    gym_env = gym.make(env_name)
+    actor = Actor(
+        input_dim=32*32,
+        action_space=gym_env.action_space,
+        repeat=3
+    )
+    input = torch.rand((batch_size, 32*32))
+    action_1 = actor(input)
+    input = torch.rand((batch_size, 32*32))
+    action_2 = actor(input)
+    input = torch.rand((batch_size, 32*32))
+    action_3 = actor(input)
+    input = torch.rand((batch_size, 32*32))
+    action_4 = actor(input)
+
+    assert torch.all(action_1 == action_2)
+    assert torch.all(action_2 == action_3)
+    assert torch.all(action_3 != action_4)
