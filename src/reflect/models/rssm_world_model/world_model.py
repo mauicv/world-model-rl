cab42440bc8682c2ba282c2f92f379bcf92becc8
Alex Athorne <a.thornysort@gmail.com>
2024-08-09 13:53:37 +0100

    Add save and load functionality

--- src/reflect/models/rssm_world_model/world_model.py
+++ src/reflect/models/rssm_world_model/world_model.py
@@ -54,6 +54,15 @@ def observation_loss(obs, decoded_obs):
 
 
 class WorldModel(torch.nn.Module):
+    model_list = [
+        'encoder',
+        'decoder',
+        'dynamic_model',
+        'reward_model',
+        'done_model',
+        'opt'
+    ]
+
     def __init__(
             self,
             encoder: ConvEncoder,
@@ -166,3 +175,36 @@ class WorldModel(torch.nn.Module):
             dones=dones,
             observations=obs
         )
+
+    def load(
+            self,
+            path,
+            name="world-model-checkpoint.pth",
+            targets=None
+        ):
+        device = next(self.parameters()).device
+        checkpoint = torch.load(
+            f'{path}/{name}',
+            map_location=torch.device(device)
+        )
+        if targets is None: targets = self.model_list
+        
+        for target in targets:
+            print(f'Loading {target}...')
+            getattr(self, target).load_state_dict(
+                checkpoint[target]
+            )
+
+    def save(
+            self,
+            path,
+            name="world-model-checkpoint.pth",
+            targets=None
+        ):
+        if targets is None: targets = self.model_list
+        
+        checkpoint = {
+            target: getattr(self, target).state_dict()
+            for target in targets
+        }
+        torch.save(checkpoint, f'{path}/{name}')
