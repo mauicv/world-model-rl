a19114266cafb58eaeaeb49023a23802266f5d42
Alex Athorne <a.thornysort@gmail.com>
2024-09-23 12:59:24 +0100

    Add layernorm to actor and critic

--- src/reflect/components/trainers/value/critic.py
+++ src/reflect/components/trainers/value/critic.py
@@ -1,7 +1,5 @@
 import torch
 import torch.nn.functional as F
-WEIGHTS_FINAL_INIT = 3e-1
-BIAS_FINAL_INIT = 3e-2
 
 
 class ValueCritic(torch.nn.Module):
@@ -14,28 +12,19 @@ class ValueCritic(torch.nn.Module):
         layers = []
         layers.extend([
             torch.nn.Linear(self.state_dim, hidden_dim),
+            torch.nn.LayerNorm(hidden_dim),
             torch.nn.ReLU()
         ])
         for _ in range(num_layers - 1):
             layers.extend([
                 torch.nn.Linear(hidden_dim, hidden_dim),
+                torch.nn.LayerNorm(hidden_dim),
                 torch.nn.ReLU()
             ])
 
         final_layer = torch.nn.Linear(hidden_dim, 1)
         layers.append(final_layer)
         self.layers = torch.nn.Sequential(*layers)
-
-        torch.nn.init.uniform_(
-            final_layer.weight,
-            -WEIGHTS_FINAL_INIT,
-            WEIGHTS_FINAL_INIT
-        )
-        torch.nn.init.uniform_(
-            final_layer.bias,
-            -BIAS_FINAL_INIT,
-            BIAS_FINAL_INIT
-        )
 
     def forward(self, x):
         return self.layers(x)
