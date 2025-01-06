a40bc46e5cf4f119e90f425ef4871bfcb5139058
Alex Athorne <a.thornysort@gmail.com>
2025-01-06 15:53:40 +0000

    Add transformer world model back in for backwards compat

--- src/reflect/components/transformer_world_model/world_model_actor.py
+++ src/reflect/components/transformer_world_model/world_model_actor.py
@@ -1,9 +1,11 @@
 from typing import Union
+import torch
+
 from reflect.components.models.actor import Actor
 from reflect.components.models.encoder import Encoder
 from reflect.components.rssm_world_model.models import DenseModel
+from reflect.components.transformer_world_model.world_model import WorldModel
 from reflect.utils import FreezeParameters
-import torch
 
 
 class EncoderActor(torch.nn.Module):
@@ -31,6 +33,34 @@ class EncoderActor(torch.nn.Module):
 
         with FreezeParameters([self.encoder, self.actor]):    
             z, _ = self.encoder(obs)
+            z = z.reshape(1, 1, -1)
+            return self.actor(z, deterministic=True)
+
+
+class TransformerWorldModelActor:
+    def __init__(
+            self,
+            world_model: WorldModel,
+            actor: Actor
+        ):
+        self.world_model = world_model
+        self.actor = actor
+
+    def reset(self):
+        pass
+
+    def __call__(self, obs: torch.Tensor) -> torch.Tensor:
+        """
+        s_{t} = encoder(o_{t})
+        a_{t} = actor(s_{t})
+        """
+        device = next(self.actor.parameters()).device
+        if obs.dim() == 4:
+            obs = obs.unsqueeze(1)
+        obs = obs.to(device)
+
+        with FreezeParameters([self.world_model, self.actor]):    
+            z, _ = self.world_model.encode(obs)
             z = z.reshape(1, 1, -1)
             return self.actor(z, deterministic=True)
             
