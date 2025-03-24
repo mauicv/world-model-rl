96a200beeb9a78458650ff592161ba1ada9ba8cb
Alex Athorne <a.thornysort@gmail.com>
2025-03-24 13:00:18 +0000

    Add priority sampling functionality

--- src/reflect/data/tests/test_data_loader.py
+++ src/reflect/data/tests/test_data_loader.py
@@ -74,6 +74,38 @@ def test_data_loader_states(env_name):
 @pytest.mark.parametrize("env_name", [
     "Ant-v4",
 ])
+def test_data_loader_priority_sampling(env_name):
+    num_time_steps = 18
+    env = gym.make(env_name, render_mode="rgb_array")
+    action_shape, = env.action_space.shape
+    data_loader = EnvDataLoader(
+        num_time_steps=num_time_steps + 1,
+        state_shape=(27,),
+        # transforms=Compose([Resize((64, 64))]),
+        env=env,
+        use_imgs_as_states=False,
+        priority_sampling_temperature=1
+    )
+    for _ in range(4):
+        data_loader.perform_rollout()
+    for i in range(4):
+        assert data_loader.end_index[i] >= num_time_steps, f'{data_loader.end_index=}'
+        assert torch.all(data_loader.state_buffer[i, data_loader.end_index[i]+1:] == 0)
+        assert torch.all(data_loader.action_buffer[i, data_loader.end_index[i]+1:] == 0)
+        assert torch.all(data_loader.reward_buffer[i, data_loader.end_index[i]+1:] == 0)
+
+    s, a, r, d = data_loader.sample(batch_size=3, num_time_steps=10, use_priority_sampling=True)
+
+    assert s.shape == (3, 10, 27)
+    assert a.shape == (3, 10, action_shape)
+    assert r.shape == (3, 10, 1)
+    assert d.shape == (3, 10, 1)
+    data_loader.close()
+
+
+@pytest.mark.parametrize("env_name", [
+    "Ant-v4",
+])
 def test_data_loader_weight_perturbation(env_name):
     num_time_steps = 18
     env = gym.make(env_name, render_mode="rgb_array")
