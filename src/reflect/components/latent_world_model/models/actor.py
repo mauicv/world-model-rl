b17f0e980ce20670aa91db0f7a1f61c5993f378b
Alex Athorne <a.thornysort@gmail.com>
2026-04-17 10:09:11 +0100

    Add uniform_ init to output layer of MLP actor

--- src/reflect/components/latent_world_model/models/actor.py
+++ src/reflect/components/latent_world_model/models/actor.py
@@ -1,6 +1,8 @@
 import torch
 import torch.nn as nn
 
+WEIGHTS_FINAL_INIT = 3e-3
+BIAS_FINAL_INIT = 3e-4
 
 class MLPActor(nn.Module):
     def __init__(self, latent_dim: int, action_dim: int, num_layers: int = 2, hidden_dim: int = 512):
@@ -18,6 +20,16 @@ class MLPActor(nn.Module):
             ])
         layers.append(nn.Linear(hidden_dim, action_dim))
         self.layers = nn.Sequential(*layers)
+        torch.nn.init.uniform_(
+            self.layers[-1].weight,
+            -WEIGHTS_FINAL_INIT,
+            WEIGHTS_FINAL_INIT
+        )
+        torch.nn.init.uniform_(
+            self.layers[-1].bias,
+            -BIAS_FINAL_INIT,
+            BIAS_FINAL_INIT
+        )
 
     def forward(self, z: torch.Tensor, deterministic: bool = True) -> torch.Tensor:
         return torch.tanh(self.layers(z))
