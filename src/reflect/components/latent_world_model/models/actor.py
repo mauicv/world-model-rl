a858d6e5a175079ed907da0f8ccc8933ec6d0ea8
Alex Athorne <a.thornysort@gmail.com>
2026-04-18 20:37:35 +0100

    Refactor the actor model

--- src/reflect/components/latent_world_model/models/actor.py
+++ src/reflect/components/latent_world_model/models/actor.py
@@ -1,35 +1,25 @@
 import torch
 import torch.nn as nn
 
-WEIGHTS_FINAL_INIT = 3e-3
+from reflect.components.latent_world_model.models.mlp import MLP, orthogonal_init_fn
 BIAS_FINAL_INIT = 3e-4
 
 class MLPActor(nn.Module):
     def __init__(self, latent_dim: int, action_dim: int, num_layers: int = 2, hidden_dim: int = 512):
         super().__init__()
-        layers = [
-            nn.Linear(latent_dim, hidden_dim),
-            nn.LayerNorm(hidden_dim),
-            nn.Mish(),
-        ]
-        for _ in range(num_layers - 1):
-            layers.extend([
-                nn.Linear(hidden_dim, hidden_dim),
-                nn.LayerNorm(hidden_dim),
-                nn.Mish(),
-            ])
-        layers.append(nn.Linear(hidden_dim, action_dim))
-        self.layers = nn.Sequential(*layers)
-        torch.nn.init.uniform_(
-            self.layers[-1].weight,
-            -WEIGHTS_FINAL_INIT,
-            WEIGHTS_FINAL_INIT
+        self.mlp = MLP(
+            input_dim=latent_dim,
+            output_dim=hidden_dim,
+            num_layers=2,
+            hidden_dim=hidden_dim,
+            output_activation=nn.Tanh,
         )
-        torch.nn.init.uniform_(
-            self.layers[-1].bias,
-            -BIAS_FINAL_INIT,
-            BIAS_FINAL_INIT
+        self.output_layer = torch.nn.Sequential(
+            nn.Linear(hidden_dim, action_dim),
+            nn.LayerNorm(action_dim),
+            nn.Tanh(),
         )
+        self.apply(orthogonal_init_fn)
 
     def forward(self, z: torch.Tensor, deterministic: bool = True) -> torch.Tensor:
-        return torch.tanh(self.layers(z))
+        return self.output_layer(self.mlp(z))
